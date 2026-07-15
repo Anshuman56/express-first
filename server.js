1167e4cf3f1425c1b1f6ae2d7cd84e5929fc7acd
Anshuman56 <anshumandehury@yandex.com>
2026-07-15 13:59:07 +0530

    build: my first post methord and send a json

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 const port = 3000;
 const now = new Date();
 
+app.use(express.json());
+// Custom logger middleware
+const requestLogger = (req, res, next) => {
+  // Log request details
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.originalUrl}`);
+
+  next(); // Pass control to the next middleware
+};
+
+app.use(requestLogger);
+
 app.get("/", (req, res) => {
   res.send("Welcome to my API");
 });
@@ -13,6 +24,10 @@ app.get("/about", (req, res) => {
 
 app.get("/time", (req, res) => {
   res.send({ hour: now.getHours(), minutes: now.getMinutes() });
+});
+
+app.post("/echo", (req, res) => {
+  res.send(`you send: ${JSON.stringify(req.body)}`);
 });
 
 app.listen(port, () => {
